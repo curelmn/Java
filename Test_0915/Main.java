9aedeb4b9c96218fcece0987843f3cda91f364e0
curelmn <1874797937@qq.com>
2021-09-15 20:56:54 +0800

    笔试

---
+++ Test_0915/Main.java
@@ -0,0 +1,80 @@
+package Test_0915;
+
+import org.apache.hadoop.shaded.com.google.inject.internal.cglib.core.$ObjectSwitchCallback;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+//    public static void main(String[] args) {
+//        Scanner scanner=new Scanner(System.in);
+//        while (scanner.hasNext()){
+//            String s=scanner.nextLine();
+//            String[] arr=s.split(",");
+//            int  target=scanner.nextInt();
+//            int len=arr.length;
+//            int num=0;
+//            int[] array=new int[len];
+//            int index=0;
+//            for (int i=0;i<len;i++){
+//                array[i]=Integer.parseInt(arr[i]);
+//            }
+//            int result=getminshops(array,target);
+//            System.out.println(result);
+//        }
+//    }
+//
+//    private static int getminshops(int[] array, int target) {
+//        int len=array.length;
+//        int[] dp=new int[target+1];
+//        dp[0]=0;
+//        boolean flag=false;
+//        for (int i=1;i<=target;i++){
+//            for (int j=0;j<len;j++){
+//                if (i-array[j]>=0){
+//                    dp[i]=dp[i-array[j]]+1;
+//                }else {
+//                    flag=true;
+//                }
+//            }
+//        }
+//        if (!flag) {
+//            return dp[target];
+//        }
+//        return -1;
+//    }
+//public static void main(String[] args) {
+//
+//        String s="1,2,3;4,5,6";
+//     String ss=   s.replace(';',',');
+//    System.out.println(ss);
+//
+//
+//
+//}
+//public static void main(String[] args) {
+//    Scanner scanner=new Scanner(System.in);
+//    while (scanner.hasNext()){
+//        String s=scanner.nextLine();
+//        String ss=s.replace(';',',');
+//        String[] arr=ss.split(",");
+//        int[] array=new int[arr.length];
+//        for (int i=0;i<arr.length;i++){
+//            array[i]=Integer.parseInt(arr[i]);
+//        }
+//        if (array.length<4){
+//            System.out.println(0);
+//        }
+//        Arrays.sort(array);
+//        System.out.println(array[3]);
+//    }
+//}
+  public static void main(String[] args) {
+    Scanner scanner=new Scanner(System.in);
+    while (scanner.hasNext()){
+        String s1=scanner.nextLine();
+        String s2=scanner.nextLine();
+        
+    }
+}
+}
